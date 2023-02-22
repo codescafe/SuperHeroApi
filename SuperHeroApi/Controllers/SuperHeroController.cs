babb7d6974ca7e6032db65d44383faebdd226223
Rio <C-primer@outlook.com>
2023-02-22 23:02:37 +0800

    Query single hero.

--- SuperHeroApi/Controllers/SuperHeroController.cs
+++ SuperHeroApi/Controllers/SuperHeroController.cs
@@ -8,21 +8,40 @@ namespace SuperHeroApi.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
-        [HttpGet]
-        public async Task<IActionResult> GetAllHeroes()
-        {
-            var superHeroes = new List<SuperHero> {
-                new SuperHero 
-                {   
-                    Id = 1, 
+        private static List<SuperHero> superHeroes = new List<SuperHero>(){
+                new SuperHero
+                {
+                    Id = 1,
                     Name ="Spider Man",
                     FirstName="Peter",
                     LastName="Parker",
                     Place="New York City"
+                },
+                new SuperHero
+                {
+                    Id = 2,
+                    Name ="Iron Man",
+                    FirstName="Tony",
+                    LastName="Stark",
+                    Place="Malibu"
                 }
             };
 
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
             return Ok(superHeroes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetSingleHeroes(int id)
+        {
+            var hero = superHeroes.Find(x => x.Id == id);
+            if(hero is null)
+            {
+                return NotFound("Sorry, But this hero doesn't exist.");
+            }
+            return Ok(hero);
         }
     }
 }
